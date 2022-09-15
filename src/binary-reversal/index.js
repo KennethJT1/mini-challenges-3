2a992f40bb45cefe40bc19afa47909dcb9fb8110
KennethJT1 <oluwatomiwa96@gmail.com>
2022-09-15 10:22:35 +0100

    w0c3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,15 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
-
-module.exports = binaryReversal;
+ function binaryReversal(value) {
+    let binary = parseInt(value).toString(2);
+    let paddedBinary =
+      binary.length <= 8
+        ? "0".repeat(8 - binary.length) + binary
+        : "0".repeat(8 - (binary.length % 8)) + binary;
+    let reversedPaddedBinary = paddedBinary.split("").reverse().join("");
+    return parseInt(reversedPaddedBinary, 2).toString();
+  }
+  return binaryReversal
+  module.exports = binaryReversal;
+  
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,5 +1,21 @@
 function listSorting(needle, haystack) {
-    let 
-}
-
-module.exports = listSorting;
+    let rowIndex = haystack.length - 1;
+    let colIndex = -1;
+  
+    for (let i = haystack.length - 1; i >= 0; i--) {
+      if (Array.isArray(haystack[i])) {
+        rowIndex = i;
+        colIndex = haystack[i].lastIndexOf(needle);
+        if (colIndex != -1) {
+          return [rowIndex, colIndex];
+        }
+      } else {
+        if (haystack[i] == needle) return i;
+      }
+    }
+    return -1;
+  }
+  return listSorting
+  
+  module.exports = listSorting;
+  
